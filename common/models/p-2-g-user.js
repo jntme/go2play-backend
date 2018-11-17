a4477578ce096b0acd706ffc572310f1f8a04c40
kybup1 <kybup1@bfh.ch>
2018-11-18 00:17:34 +0100

    Friends can be added to a user
    username is know the primary key

--- common/models/p-2-g-user.js
+++ common/models/p-2-g-user.js
@@ -15,6 +15,32 @@ module.exports = function(P2guser) {
           accepts: {arg: 'name', type: 'string', required: true, http: { source: 'query' } },
           returns: {arg: 'userCount', type: 'number'}
         }
-    );
+  );
+
+  P2guser.addFriend = function(username, nFriend, cb) {
+
+    P2guser.findOne({where: {'name':username}})
+    .then(function(user){
+      user.friends.add(nFriend)
+      .catch(function(err){
+        return cb(null, "false")
+      })
+      return cb(null, "true")
+    })
+  };
+
+  P2guser.remoteMethod (
+    'addFriend',
+    {
+      http: {path: '/addFriend/'},
+      accepts: [
+                  {arg: "username", type: "string", riquired:true},
+                  {arg: 'nFriend', type: 'string', required: true}
+                ],
+      returns: {arg: 'ok', type: 'string'}
+    }
+);
+
+
 
 };
