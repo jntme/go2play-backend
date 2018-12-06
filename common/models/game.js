6e8750a5201222594c87c70127fa5461b3d17dfb
Jonathan Meier <mail@jntme.ch>
2018-12-06 11:35:26 +0100

    change round to array of objects

--- common/models/game.js
+++ common/models/game.js
@@ -1,45 +1,49 @@
 'use strict';
 
-module.exports = function(Game) {
+module.exports = function (Game) {
 
-    Game.new = function(username, friend, cb) {
-        var app = Game.app
-        var P2guser = app.models.P2GUser;
-        var Question = app.models.Question;
-        
-        var promUser1 = P2guser.findOne({where: {'name':username}})
-        var promUser2 = P2guser.findOne({where: {'name':friend}})
-        var promQuestion = Question.find()
+  Game.new = function (username, friend, cb) {
+    var app = Game.app
+    var P2guser = app.models.P2GUser;
+    var Question = app.models.Question;
 
-        Promise.all([promUser1, promUser2, promQuestion]).then(function(users) {
-            var game = new app.models.Game()
-            var questions = users[2]
+    var promUser1 = P2guser.findOne({where: {'name': username}})
+    var promUser2 = P2guser.findOne({where: {'name': friend}})
+    var promQuestion = Question.find()
 
-            game.user1(users[0])
-            game.user2(users[1])
-            game.save().then(function(game) {
-                users[0].games.add(game)
-                users[1].games.add(game)
-                return game
-            }).then(function(game) {
-                
-                game.rounds.create({"finished":"false"})
-                
-               return cb(null, game.id)
-            })
-            
-        })
-    };
+    Promise.all([promUser1, promUser2, promQuestion]).then(function (users) {
+      var game = new app.models.Game()
+      var questions = users[2]
 
-    Game.remoteMethod (
-        'new',
-        {
-          http: {path: '/new/'},
-          accepts: [
-                      {arg: "username", type: "string", required:true},
-                      {arg: 'friend', type: 'string', required: true}
-                    ],
-          returns: {arg: 'id', type: 'string'}
-        }
-    )
+      game.user1(users[0])
+      game.user2(users[1])
+
+      game.rounds = [{
+        'name': 'test',
+        'name2': 'test2'
+      }];
+
+      game.save().then(function (game) {
+        users[0].games.add(game)
+        users[1].games.add(game)
+        return game
+      }).then(function (game) {
+        return cb(null, game.id)
+
+      })
+
+    })
+  };
+
+  Game.remoteMethod(
+    'new',
+    {
+      http: {path: '/new/'},
+      accepts: [
+        {arg: "username", type: "string", required: true},
+        {arg: 'friend', type: 'string', required: true}
+      ],
+      returns: {arg: 'id', type: 'string'}
+    }
+  )
 }
