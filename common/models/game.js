7f0427368339df92bbdd4d88dadb5d53286177e9
Jonathan Meier <mail@jntme.ch>
2018-12-25 11:54:26 +0100

    make game winnable =)

--- common/models/game.js
+++ common/models/game.js
@@ -152,7 +152,12 @@ module.exports = function (Game) {
         }
 
         //checks if the questions are form the correct round and if the correct user is playing
-        if (roundNr != game.activeRound || username != game.activeUser) {
+        if (game.activeUser == 'game finished') {
+          let err = new Error("Game already finished.");
+          err.statusCode = 404;
+          return cb(err);
+        }
+        else if (roundNr != game.activeRound || username != game.activeUser) {
           return cb(null, {"error": "Wrong user or round"})
         }
         //checks if there are only three answers
@@ -178,39 +183,96 @@ module.exports = function (Game) {
           game = nextRound(game, isUser1);
 
           game.save();
-          return cb(null, {"ok": "true"})
+
+          if (game.activeUser !== 'game finished') {
+            return cb(null, game)
+          }
+          else {
+            return cb(null, game)
+          }
+
         }
       })
   };
 
+  //checks four different cases in which a game can be in and manipulates the object accordingly
   let nextRound = function (game, isUser1) {
-    // todo this function is not understandable for a moderately intelligent human being
-    //checks four different cases in which a game can be in and manipulates the object accordingly
+    const MAX_ROUNDS = 3; // if you change this number you need to change the following if/else-clause as well!
+
+    // a game with three rounds proceeds in the following order:
+    // user: gfels    friend: jntme (example)
+    //
+    // activeUser: jntme   activeRound: 1 (the friend begins)
+    // activeUser: gfels   activeRound: 1
+    // activeUser: gfels   activeRound: 2
+    // activeUser: jntme   activeRound: 2
+    // activeUser: jntme   activeRound: 3
+    // activeUser: gfels   activeRound: 3
 
     if (!isUser1 && !(game.activeRound % 2) == 0) {
       game.activeUser = game.user1;
     } else if (isUser1 && !(game.activeRound % 2) == 0) {
-      console.log('max rounds:' + game.rounds[game.rounds.length-1].roundNumber);
-      console.log('actual round:' + game.activeRound);
-      game.activeRound += 1;
+      if (game.activeRound >= MAX_ROUNDS) {
+        game.activeUser = 'game finished';
+        game = gameFinished(game);
+      } else {
+        game.activeRound += 1;
+      }
     } else if (isUser1 && (game.activeRound % 2) == 0) {
       game.activeUser = game.user2;
     } else if (!isUser1 && (game.activeRound % 2) == 0) {
-      console.log('max rounds:' + game.rounds[game.rounds.length-1].roundNumber);
-      console.log('actual round:' + game.activeRound);
       game.activeRound += 1;
-
     }
+
     return game;
   }
 
-  // todo add that game is finished, after the last round is being played
-  // if(game.rounds[game.rounds.length-1].roundNumber > game.activeRound)  {
-  //   game.activeRound += 1;
-  // }
-  // else {
-  //   game.activeRound = "game finished."
-  // }
+  //calculates the winner of the game and updates that users totalscore
+  let gameFinished = function (game) {
+    let app = Game.app;
+    let P2guser = app.models.P2GUser;
+
+    let scorePlayer1 = 0;
+    let scorePlayer2 = 0;
+
+    game.rounds.forEach(round => {
+      round.gameQuestions.forEach(question => {
+        if (question[question.user1] == 1) scorePlayer1++;
+        if (question[question.user2] == 1) scorePlayer2++;
+      });
+    });
+
+    console.log('scorePlayer1:', scorePlayer1);
+    console.log('scorePlayer2:', scorePlayer2);
+
+    let winningUsers = [];
+
+    if (scorePlayer1 > scorePlayer2 || scorePlayer1 == scorePlayer2) {
+      winningUsers.push(P2guser.findOne({where: {'name': game.user1}}));
+      game.winner = game.user1;
+      console.log("added user1 to winners");
+    }
+    if (scorePlayer2 > scorePlayer1 || scorePlayer1 == scorePlayer2) {
+      winningUsers.push(P2guser.findOne({where: {'name': game.user2}}));
+      game.winner = game.user2;
+      console.log("added user2 to winners");
+    }
+
+    // if both won, overwrite with 'both'
+    if(winningUsers.length == 2) game.winner = "both";
+
+    Promise.all(winningUsers).then(users => {
+      users.forEach(user => {
+        console.log('found fucking user:', user);
+        console.log("updated the total score for user " + user.name);
+
+        user.totalscore++;
+        user.save();
+      });
+    });
+
+    return game;
+  }
 
   Game.remoteMethod(
     'answer',
